86f076c52e0a8ee03fb8d856737e863d5fc5e535
ProbeDream <Gump1016@163.com>
2019-10-17 14:07:19 +0800

    TS中的类型推断!

---
+++ src/advanced.ts
@@ -0,0 +1,18 @@
+let a = 1;
+let c = {x: 1, y: 'a'}
+
+let d = (x = 1) => x + 1
+
+window.onkeydown = (event:KeyboardEvent) => {
+    // console.log(event.button)
+}
+
+interface Foo {
+    bar: number
+}
+// let foo = {} as Foo
+// let foo = <Foo>{}
+let foo: Foo = {
+    bar: 1
+}
+// foo.bar = 1
